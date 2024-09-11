15d44515f41ff0d8e5830c00c21041e69a606f5c
Jesús Flores <a.22308051281186@cbtis128.edu.mx>
2024-09-11 07:49:50 -0600

    Add files via upload

---
+++ mi_conjunto.py
@@ -0,0 +1,7 @@
+print("Ejemplo de set")
+tipodeportes={"Futbol","Tenis","Basquetbol"}
+print(tipodeportes)
+
+print("Lista de elementos del conjunto tipodeportes loop for")
+for flores in tipodeportes:
+    print(flores)
---
+++ mi_diccionario.py
@@ -0,0 +1,15 @@
+print("Ejemplo de diccionario")
+
+canciones = {
+  "Billie jean": "Michael Jackson",
+  "Ya lo se": "Juan Gabriel",
+  "Secreto de amor": "Joan Sebastian"
+}
+print(canciones)
+
+print( canciones ["Billie jean"])
+
+print("Lista de elementos con loop for")
+
+for flores in canciones:
+    print(flores)
